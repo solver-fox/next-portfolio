8dc377a141d74260d9c782c77c93856366be58fe
Charles Hardy <charleshardy1225@gmail.com>
2023-11-29 04:35:32 -0600

    image updated

--- src/contents/career.js
+++ src/contents/career.js
@@ -3,31 +3,31 @@ export const careers = {
         title: "Senior Full Stack Developer (Emonics, LLC)",
         abstract: `Spearheaded the development of a delivery web application, contributing to a 15% increase in revenue and a 20% boost in user engagement within the first two months. Collaborated with designers and product owners to establish robust security platforms, ensuring protection against potential threats and vulnerabilities. Mentored and coached junior developers, elevating team satisfaction by 10% and enhancing overall productivity.`,
         date: '2023-10-30',
-        banner: '/static/hello-world-banner.jpg',
+        banner: '/static/3d-abstract-background-with-neon-lights.jpg',
     },
     "Merge": {
         title: "Full Stack Engineer (Merge)",
         abstract: `Engineered an Apollo Server with Express to handle GraphQL requests, resulting in a 15% improvement in overall application speed. Led the optimization of the frontend codebase and database schema, contributing to the development of a responsive web-based application. Successfully resolved a critical production issue by conducting root cause analysis, collaborating with cross-functional teams, and implementing a solution, which resulted in a significant reduction in downtime.`,
         date: '2021-07-10',
-        banner: '/static/hello-world-banner.jpg'
+        banner: '/static/modern-futuristic-sci-fi-background.jpg'
     },
     "S&P Global": {
         title: "Database Developer (S&P Global)",
         abstract: `Designed optimized database schema and RESTful APIs, significantly improving data retrieval speed. Developed a high-performance backend system, resulting in a 15% increase in conversion rates and website traffic. Implemented Agile methodologies in project management, enabling the timely delivery of three major projects within tight deadlines. Collaborated with the DevOps team to implement CI/CD practices, resulting in reduced software release times.`,
         date: '2019-04-30',
-        banner: "/static/hello-world-banner.jpg"
+        banner: "/static/realistic-neon-lights-background.jpg"
     },
     "Chewy": {
         title: "Frontend Developer (Chewy)",
         abstract: "Implemented unit tests and an automated testing platform, reducing bugs and enhancing overall project quality. Collaborated with the UI/UX team to define and implement a user-friendly experience, resulting in increased customer signups.",
         date: '2017-08-30',
-        banner: "/static/hello-world-banner.jpg"
+        banner: "/static/wavy-background-concept.jpg"
     },
     "Anduril": {
         title: "Full Stack Developer (Anduril)",
         abstract: `Developed and deployed an e-commerce web application using the MERN Stack. Optimized applications, reducing page loading time and improving system performance by leveraging modern web technologies.`,
         date: '2016-11-01',
-        banner: "/static/hello-world-banner.jpg",
+        banner: "/static/modern-react-css-banner.jpg",
     },
     "Workwell": {
         title: "Web Developer (Workwell)",
@@ -39,12 +39,12 @@ export const careers = {
         title: "Freelance Developer (oDesk)",
         abstract: `Developed diverse web-based applications, enhancing user experience and usability. Collaborated closely with design teams, resulting in successful project updates.`,
         date: '2013-04-20',
-        banner: "/static/hello-world-banner.jpg"
+        banner: "/static/gradient-blue-background.jpg"
     },
     "Education": {
         title: "University of North Texas",
         abstract: "Bachelor's degree in Computer and information sciences",
         date: '2012-05-30',
-        banner: "/static/hello-world-banner.jpg"
+        banner: "/static/glowing-pink-neon-frame.jpg"
     }
 };
